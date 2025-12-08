e0462b099d8c0578ec5b10e7dfa119c677ded752
19-SDEV-DEV-JECA <19-SDEV-DEV-JECA@19-SDEV-DEV-JECA.local>
2025-12-08 15:17:17 +0800

    added a new splash screen and update the build gradle but it is still cannot build apk.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,14 +1,20 @@
 plugins {
     id("com.android.application")
     id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
 
 android {
     namespace = "com.example.flutter_demo"
-    compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    compileSdk = 36      // update to latest stable, >=35 for flutter_native_splash
+
+    defaultConfig {
+        applicationId = "com.example.flutter_demo"
+        minSdk = flutter.minSdkVersion
+        targetSdk = 36    // also update targetSdk
+        versionCode = 1
+        versionName = "1.0.0"
+    }
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -19,21 +25,8 @@ android {
         jvmTarget = JavaVersion.VERSION_11.toString()
     }
 
-    defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.flutter_demo"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode
-        versionName = flutter.versionName
-    }
-
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
